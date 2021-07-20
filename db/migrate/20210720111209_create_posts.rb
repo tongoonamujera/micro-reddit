f643d1cec34d3acb858fdbe34eac76dc77de0661
tongoonamujera <tongoonamujera@gmail.com>
2021-07-20 13:14:53 +0200

    Add User and Post model

---
+++ db/migrate/20210720111209_create_posts.rb
@@ -0,0 +1,9 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.references :User, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
