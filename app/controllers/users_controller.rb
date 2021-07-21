18a09bb3e0a42e95c05e6b95a4361b3e17082f8e
tongoonamujera <tongoonamujera@gmail.com>
2021-07-21 07:24:48 +0200

    Fix webpacker issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,7 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.fetch(:user, {})
+      # params.fetch(:user, {})
+      params.require(:user).permit(:userName)
     end
 end
