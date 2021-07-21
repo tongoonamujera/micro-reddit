91630e3324ca8c5503d1577a483534fc28fbf557
tongoonamujera <tongoonamujera@gmail.com>
2021-07-21 14:10:26 +0200

    Add migrations to post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_115222) do
+ActiveRecord::Schema.define(version: 2021_07_21_120352) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2021_07_20_115222) do
     t.integer "User_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "body"
     t.index ["User_id"], name: "index_posts_on_User_id"
   end
 
