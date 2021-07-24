3e587454ce89c939cfd482a7ae71159a1fe7388a
tongoonamujera <tongoonamujera@gmail.com>
2021-07-24 20:06:41 +0200

    Modify routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :comments
-  resources :posts
+  resources :comments do
+    resources :posts
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
