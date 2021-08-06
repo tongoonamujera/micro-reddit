62595d1b2f6e422463ecf754e83e012448723d24
tongoonamujera <tongoonamujera@gmail.com>
2021-08-06 19:33:42 +0200

    Add an associations between user and comment models

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_121651) do
+ActiveRecord::Schema.define(version: 2021_08_06_172854) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -42,5 +44,6 @@ ActiveRecord::Schema.define(version: 2021_08_06_121651) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
