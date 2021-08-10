a04d397386804245238eefc929a662a84910a4ac
tongoonamujera <tongoonamujera@gmail.com>
2021-08-10 21:22:44 +0200

    Add image field and add more styles

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
   validates :title, :body, presence:true
+  mount_uploader :image, FileUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_172854) do
+ActiveRecord::Schema.define(version: 2021_08_10_184848) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_08_06_172854) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
